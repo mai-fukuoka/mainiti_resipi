c08ffa92e61963bd5889be6b6189bda387110cf2
mai-fukuoka <m.fukuoka02@gmail.com>
2021-05-30 06:09:34 +0900

    Add Myresipi class

--- src/models/Myresipi.java
+++ src/models/Myresipi.java
@@ -1,5 +1,13 @@
 package models;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.Lob;
+import javax.persistence.ManyToOne;
 import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
 import javax.persistence.Table;
@@ -16,6 +24,55 @@ import javax.persistence.Table;
             ),
 })
 
+@Entity
 public class Myresipi {
+    @Id
+    @Column(name="id")
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Integer id;
+
+    @ManyToOne
+    @JoinColumn(name="user_id",nullable=false)
+    private User user;
+
+    @Column(name="title",length=255,nullable=false)
+    private String title;
+
+    @Lob
+    @Column(name="content",nullable=false)
+    private String content;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
 
 }
