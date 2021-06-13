fe5dba3a9abef4bc1ddb7302544a0c6dd7205e67
mai-fukuoka <m.fukuoka02@gmail.com>
2021-06-13 16:45:32 +0900

    Update Css

--- WebContent/WEB-INF/views/layout/app.jsp
+++ WebContent/WEB-INF/views/layout/app.jsp
@@ -8,27 +8,31 @@
 
     <div id="wrapper">
         <div id="header">
-                <div class="title" >
-                    <a href="<c:url value='/'/>">まいにちレシピ</a>
-                </div>
-                           <div id="header-menus">
-                <div id="login">
-                    <c:choose>
-                        <c:when test="${sessionScope.login_user !=null }">
-                            <a href="<c:url value='/logout' />">ログアウト</a>
-                        </c:when>
-                        <c:otherwise>
-                            <a href="<c:url value='/login' />">ログイン</a>
-                        </c:otherwise>
-                    </c:choose>
+            <div class="title">
+                <a href="<c:url value='/'/>">まいにちレシピ</a>
+            </div>
+            <div id="header-menu-area">
+                <div id="header-menus">
+                    <div id="login">
+                        <c:choose>
+                            <c:when test="${sessionScope.login_user !=null }">
+                                <a href="<c:url value='/logout' />">ログアウト</a>
+                            </c:when>
+                            <c:otherwise>
+                                <a href="<c:url value='/login' />">ログイン</a>
+                            </c:otherwise>
+                        </c:choose>
                     </div>
                     &nbsp;&nbsp;
                     <div id="new">
-                            <a href="<c:url value='/users/new'/>">新規登録</a>
+                        <a href="<c:url value='/users/new'/>">新規登録</a>
                     </div>
+                </div>
             </div>
         </div>
-        <div id="content">${param.content}</div><br/><br/>
+        <div id="content">${param.content}</div>
+        <br />
+        <br />
         <div id="footer">by MAI FUKUOKA.</div>
     </div>
 
---
+++ src/models/Kaimonolist.java
@@ -0,0 +1,69 @@
+package models;
+
+import javax.persistence.Column;
+import javax.persistence.ColumnResult;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedNativeQueries;
+import javax.persistence.NamedNativeQuery;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.SqlResultSetMapping;
+import javax.persistence.Table;
+
+@Table(name = "kaimonolists")
+@SqlResultSetMapping(name = "deleteResult", columns = { @ColumnResult(name = "count") })
+@NamedQueries({
+    @NamedQuery(name = "getAllKaimonolists",
+                query = "SELECT k FROM Kaimonolist AS k WHERE k.userid = :userid ORDER BY k.id DESC"),
+    @NamedQuery(name = "getKaimonolistsCount",
+                query = "SELECT COUNT(k) FROM Kaimonolist AS k WHERE k.userid = :userid")
+})
+@NamedNativeQueries({
+    @NamedNativeQuery(name = "removeAllKaimonolists",
+            query = "DELETE FROM kaimonolists WHERE kaimonolists.userid = :userid",
+            resultSetMapping = "deleteResult")
+
+})
+
+@Entity
+public class Kaimonolist {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "userid", length = 255, nullable = true)
+    private String userid;
+
+    @Column(name = "ingredient", length = 255, nullable = false)
+    private String ingredient;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUserid() {
+        return userid;
+    }
+
+    public void setUserid(String userid) {
+        this.userid = userid;
+    }
+
+    public String getIngredient() {
+        return ingredient;
+
+    }
+
+    public void setIngredient(String ingredient) {
+        this.ingredient = ingredient;
+    }
+
+}
